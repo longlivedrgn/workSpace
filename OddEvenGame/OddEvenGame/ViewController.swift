cf29dea54d9493653437c2e94ed5a8a52b6a341e
김용재 <85781941+longlivedrgn@users.noreply.github.com>
2022-11-22 10:24:50 +0900

    이미지추가, 로직 개선

--- OddEvenGame/OddEvenGame/ViewController.swift
+++ OddEvenGame/OddEvenGame/ViewController.swift
@@ -8,11 +8,15 @@
  1. 컴퓨터가 1에서 10까지의 랜덤으로 숫자를 선택합니다.
  2. 사용자는 가진 구슬 개수를 걸고 홀짝 중의 하나를 선택한다.
  3. 결과값이 화면에 보여진다.
+ 
+ // 컴터랑 나랑 같은 거면 내가 이긴다!
  */
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fistImage: UIImageView!
+    @IBOutlet weak var imageContainer: UIView!
     @IBOutlet weak var resultLb1: UILabel!
     @IBOutlet weak var computerBallCountLb1: UILabel!
     @IBOutlet weak var userBallCountLb1: UILabel!
@@ -26,11 +30,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         computerBallCountLb1.text = String(comBallisCount)
         userBallCountLb1.text = String(userBallisCount)
+        // 처음 시작될 때는 fistImage가 보이지 않게 해놓는다.
+        self.imageContainer.isHidden = true
     }
 
     @IBAction func gameStartPressed(_ sender: Any) {
-        print("게임 시작!")
-        print(self.getRandom())
+        
+        self.imageContainer.isHidden = false
+        // 애니메이션 !
+        UIView.animate(withDuration: 3.0) {
+            self.fistImage.transform = CGAffineTransform(scaleX: 5, y: 5)
+            self.fistImage.transform = CGAffineTransform(scaleX: 1, y: 1)
+        } completion: { _ in
+            self.imageContainer.isHidden = true
+            self.showAlert()
+        }
+
+    }
+    
+    func showAlert(){
         // alert -> 화면 중앙, actionsheet -> 화면 하단
         let alert = UIAlertController.init(title: "GAME START", message: "홀 짝을 선택해주세요", preferredStyle: .alert)
         
@@ -43,13 +61,10 @@ class ViewController: UIViewController {
                 return
             }
             
-            guard let value = Int(input) else {
-                return
-            }
-            
             print("입력한 값은 \(input)입니다.")
             self.getWinner(count: value, select: "홀")
         }
+        
         let evenBtn = UIAlertAction(title: "짝", style: .default){ _ in
             print("짝버튼을 클릭했습니다.")
             
@@ -84,22 +99,36 @@ class ViewController: UIViewController {
         if comType == select {
             print("User win")
             result = result + "(User Win)"
+            self.resultLb1.text = result
             self.calculateBalls(winner: "user", count: count)
-        } else{
+        } else {
             result = result + "(Com Win)"
             print("Computer win")
+            self.resultLb1.text = result
             self.calculateBalls(winner: "com", count: count)
         }
-        self.resultLb1.text = result
     }
+    func checkAccountEmpty(balls: Int) -> Bool {
+        return balls == 0
+    }
+    
+    
     
     func calculateBalls(winner: String, count: Int){
         if winner == "com" {
             self.userBallisCount = self.userBallisCount - count
             self.comBallisCount = self.comBallisCount + count
+            
+            if self.checkAccountEmpty(balls: self.userBallisCount) {
+                self.resultLb1.text = "컴퓨터 최종승리"
+            }
         } else {
             self.comBallisCount = self.comBallisCount - count
             self.userBallisCount = self.userBallisCount + count
+            
+            if self.checkAccountEmpty(balls: self.comBallisCount){
+                self.resultLb1.text = "사용자 최종승리"
+            }
         }
         self.userBallCountLb1.text = "\(self.userBallisCount)"
         self.computerBallCountLb1.text = "\(self.comBallisCount)"
